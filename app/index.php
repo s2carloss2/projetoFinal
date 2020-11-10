7f1ec54bc07a61ddbddd8120637ad82e92557794
s2carloss2 <carloscarloshenrique@hotmail.com>
2020-11-10 14:34:48 -0300

    commit

---
+++ app/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app\assets\bootstrap\css/bootstrap.min.css">
+    <link rel="stylesheet" href="app\assets\icomoon/style.css">
+    <title>Projeto Final</title>
+</head>
+<body>
+
+<span class="iconadobe"></span>
+<span class="iconaddthis"></span>
+</body>
+</html>
