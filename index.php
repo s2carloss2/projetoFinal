13d24a0a31d8544c04ef94e066efdefcc31a1529
ReginaldojLima <reginaldojlima963@gmail.com>
2020-11-10 14:32:58 -0300

    Arquivos base

--- index.php
+++ index.php
@@ -1,11 +1,49 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto final</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="app/assets/bootstrap/css/bootstrap.min.css">
+
+    <!-- Icomoon -->
+    <link rel="stylesheet" href="app/assets/icomoon/style.css">
+
+    <!-- outros -->
+    <link rel="stylesheet" href="">
+
+    <!-- Meu CSS -->
+    <link rel="stylesheet" href="">
+
+
 </head>
+
 <body>
-    
+
+    <!-- Header -->
+    <!-- Fim Header -->
+
+
+    <!-- Páginas do meu Site -->
+    <!-- Fim Páginas do meu Site -->
+
+
+    <!-- Rodapé -->
+
+
+
+    <!-- jQuery -->
+    <script src="app/assets/js/jquery.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="app/assets/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Funções Javascript -->
+    <script src="app/assets/js/funcoes.js"></script>
+
 </body>
+
 </html>
